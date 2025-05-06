186177fbfa4a9394c03154f6d2d861538095b5ef
Krishna Kant Sharma <kknewsits4u@gmail.com>
2025-05-06 07:12:02 +0530

    crm_backend

--- App/app.js
+++ App/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   credentials:true,
-  origin:["https://coruscating-pothos-a5207a.netlify.app", "http://localhost:5173"],
+  origin:["https://crm-manager.netlify.app", "http://localhost:5173"],
 }))
 
 
