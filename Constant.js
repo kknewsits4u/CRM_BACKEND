89f3e047b9e8f408588c4d23220191dd481efb6c
kknewsits4u <kknewsits4u@gmail.com>
2025-05-14 23:15:14 +0530

    Update Constant.js

--- Constant.js
+++ Constant.js
@@ -2,4 +2,4 @@ export const PORT = 8585;
 
  export const DB_NAME  = "crm_project";
 
- export const DBUrlInfo =`mongodb+srv://krishnakantsharma560:Kri932392@cluster1.0gtjt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster1`;
+ export const DBUrlInfo =``;
